037d46f470dda914a8c074a137e96e08fe25cba1
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2024-12-12 16:09:51 +0530

    completed listing REST API operations

--- router/listing.js
+++ router/listing.js
@@ -2,15 +2,45 @@ const express = require('express');
 const Listing = require('../models/listing');
 const router = express.Router();
 
+// Getting all data
 router.get('/',async(req,res)=>{
     const listings = await Listing.find();
     res.send(listings);
 });
 
-
-router.post('/:id',async(req,res)=>{
+// Getting data by id
+router.get('/:id',async(req,res)=>{
     const id = req.params.id;
-    const listings = await Listing.find();
+    const listing = await Listing.findById(id);
+    res.send(listing);
+});
+
+// Adding data
+router.post('/',async(req,res)=>{
+    const {listing} = req.body;
+    const newListing = new Listing(listing);
+    await newListing.save();
+    res.send(newListing);
+});
+
+
+// Updating data
+router.put('/:id',async(req,res)=>{
+    const id = req.params.id;
+    const {listing} = req.body;
+    listing.updatedAt = new Date();
+    console.log(listing);
+    await Listing.findByIdAndUpdate(id, listing);
+    const upadetedListing = await Listing.findById(id);
+    res.send(upadetedListing);
+});
+
+// Deleting data
+router.delete('/:id',async(req,res)=>{
+    const id = req.params.id;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.send({status: 200, message: 'Deleted successfully'});
 });
 
 
